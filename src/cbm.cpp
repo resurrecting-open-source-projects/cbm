954e272fc8ec695e4f34ade2e3506ea73c3b31ef
Joao Eriberto Mota Filho <eriberto@eriberto.pro.br>
2019-02-09 00:35:58 -0200

    Add licensing headers and set rights

--- src/cbm.cpp
+++ src/cbm.cpp
@@ -1,3 +1,25 @@
+/*
+  Color Bandwidth Meter (CBM) - display in real time the network traffic speed
+
+  Copyright 2005-2006 Aaron Isotton <aaron@isotton.com>
+  Copyright 2006      Paul Martin <pm@debian.org>
+  Copyright 2012      Cyril Brulebois <kibi@debian.org>
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; version 2
+  of the License.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 #include "config.h"
 #include "ErrnoError.hpp"
 #include "statistics.hpp"
@@ -24,7 +46,7 @@
 extern int optind, opterr, optopt;
 
 namespace {
-    
+
 // Globals
 struct option longopts[] =
 {
